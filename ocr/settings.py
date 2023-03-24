ea5997dd83532062ec5a18ddcd05e1ad19568b6b
Hafimuthasir <muthasirhafi@gmail.com>
2023-03-24 10:35:56 +0530

    database changed to sqllite3 for simple usage

--- ocr/settings.py
+++ ocr/settings.py
@@ -77,13 +77,20 @@ WSGI_APPLICATION = 'ocr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ocr',
+#         'USER':'postgres',
+#         'PASSWORD':'admin',
+#         'HOST':'localhost'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ocr',
-        'USER':'postgres',
-        'PASSWORD':'admin',
-        'HOST':'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
